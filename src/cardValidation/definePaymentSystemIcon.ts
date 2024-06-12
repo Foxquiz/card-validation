3332a1b6cdf2d962b33543176c958e89a990b8c5
Arina Bardyukova <arina.bardyukova@yandex.ru>
2024-06-12 16:41:44 +0300

    adds second way to link imgs

--- src/cardValidation/definePaymentSystemIcon.ts
+++ src/cardValidation/definePaymentSystemIcon.ts
@@ -1,12 +1,17 @@
 import { cardNumber } from "card-validator-es6";
+import { paySistemsTypes } from "./pay_systems_icons";
+
 
 export function definePaymentSystemImg(inputValue: string): string {
     const cardType = cardNumber(inputValue).card;
     let imgUrl;
     if (cardType == null) {
-        imgUrl = `/unknown_system.png?url`;
+        imgUrl = `http://localhost:5173/unknown_system.png`;
         return imgUrl;
     }
-    imgUrl = `/${cardType.type}.png?url`;
+    const imgSrc = paySistemsTypes.filter((item) => {
+        return item.title === cardType.type;
+    });
+    imgUrl = `http://localhost:5173${imgSrc[0]?.imgUrl}`;
     return imgUrl;
 }
